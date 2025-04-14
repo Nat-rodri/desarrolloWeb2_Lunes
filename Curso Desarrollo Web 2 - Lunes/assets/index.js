23ba476c175bde7c319b4a19c026d0bd3929842a
Nat-rodri <natali15rodriguez@gmail.com>
2025-04-14 20:19:05 -0300

    primer commit

---
+++ Curso Desarrollo Web 2 - Lunes/assets/index.js
@@ -0,0 +1,20 @@
+//alert("Alerta desde js");
+
+console.log("Hola desde index.js");
+
+const nombreInstructor = "Natali"
+const apellidoInstructor = "Rodriguez"
+
+const nombreCompleto = nombreInstructor + " " + apellidoInstructor
+
+const nombreComMejorado = `${nombreInstructor} ${apellidoInstructor}`
+
+console.log(nombreCompleto)
+
+let edad = 18
+edad = 19
+
+console.log("mi edad es: " + edad)
+
+let edadUsuario = prompt("indicame tu edad: ")
+alert(edadUsuario)
