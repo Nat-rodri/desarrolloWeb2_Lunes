8d99ace86b3d4a4319f1a63870aea84927ada731
Nat-rodri <natali15rodriguez@gmail.com>
2025-04-21 17:05:41 -0300

    desafio_clase_02

---
+++ assets/desafio02.js
@@ -0,0 +1,8 @@
+let nombreProducto = "Marcadador indeleble";
+let precioUnitario = 100;
+
+let cantidadDeseada = prompt("¿Cuántos marcadores quiere comprar?");
+
+let costoTotal = 100*cantidadDeseada;
+
+alert(`La cantidad deseada es ${costoTotal}`)
